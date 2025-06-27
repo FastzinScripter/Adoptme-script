ee192326bb4920677141aff4f1c582e1712c5fe1
FastzinScripter <fastzin.adm@gmail.com>
2025-06-27 00:36:04 -0300

    Create farm-potions.lua

---
+++ farm-potions.lua
@@ -0,0 +1,17 @@
+task.spawn(function() 
+  script_key="RBHMKhhvloKLiPUhQlxgQcDxlyxsrzRN";
+  getgenv().Config = {
+      ["PetFarmAutoSwitchFullGrown"] = false,
+      ["PetFarmActive"] = true,
+      ["EggFarmActive"] = false,
+      ["Blur_username"] = true,
+      ["Blazing_Lion_Log"] = false,
+      ["DiscordId"] = "123456",
+      ["Webhook"] = "https://discord.com/api/webhooks/123456",
+  }
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+end)
+
+task.spawn(function() 
+  _G.Config = { UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", discord_id = "1037147029614362624" , Note = "Pc", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/adoptmee"))()
+end)
